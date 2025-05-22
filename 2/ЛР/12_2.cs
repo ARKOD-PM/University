444e4cea93cdb70750ea08428ea1cc00c506d8ef
ARKOD-PM <konstantinarkashev@gmail.com>
2025-05-22 05:54:12 +0000

    lab(12.2): find numeric palindromes

---
+++ 2/ЛР/12_2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+unsafe class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите размер массива: ");
+        int size = int.Parse(Console.ReadLine());
+
+        int* arr = stackalloc int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write($"Элемент {i + 1}: ");
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("\nПалиндромы в массиве:");
+        for (int i = 0; i < size; i++)
+        {
+            if (IsPalindrome(arr[i]))
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+
+    static bool IsPalindrome(int number)
+    {
+        if (number < 0) return false;
+        
+        int reversed = 0;
+        int original = number;
+        
+        while (original > 0)
+        {
+            reversed = reversed * 10 + original % 10;
+            original /= 10;
+        }
+        return number == reversed;
+    }
+}
