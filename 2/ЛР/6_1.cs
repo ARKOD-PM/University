12d342e10f409e2f9278be27985f178bc28c63d5
ARKOD-PM <konstantinarkashev@gmail.com>
2025-05-22 05:49:43 +0000

    lab(6.1): count mobile operators

---
+++ 2/ЛР/6_1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Phone
+{
+	public string Number { get; set; }
+	public string Operator { get; set; }
+
+	public Phone(string number, string operatorName)
+	{
+		Number = number;
+		Operator = operatorName;
+	}
+}
+
+class Program
+{
+	static void Main()
+	{
+	  Console.WriteLine("Введите количество телефонов:");
+	int n = int.Parse(Console.ReadLine());
+
+	List<Phone> phones = new List<Phone>();
+	for (int i = 0; i < n; i++)
+	{
+		Console.Write($"Номер {i+1}: ");
+		string num = Console.ReadLine();
+	
+		Console.Write($"Оператор {i+1}: ");
+		string op = Console.ReadLine();
+	
+		phones.Add(new Phone(num, op));
+	} 
+		Dictionary<string, int> operatorStats = new Dictionary<string, int>();
+
+		foreach (Phone phone in phones)
+		{
+			if (operatorStats.ContainsKey(phone.Operator))
+				operatorStats[phone.Operator]++;
+			else
+				operatorStats[phone.Operator] = 1;
+		}
+
+		Console.WriteLine("Статистика операторов:");
+		foreach (var entry in operatorStats.OrderByDescending(e => e.Value))
+		{
+			Console.WriteLine($"{entry.Key}: {entry.Value}");
+		}
+	}
+}
